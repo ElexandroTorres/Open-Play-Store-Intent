7a3628b96ca8c9233af421971581bb367a8bfdf5
ElexandroTorres <elexandro.torres@gmail.com>
2024-02-07 09:56:14 -0300

    feature: Open youtube page on playstore

--- app/src/main/java/com/elexandro/openplaystore/MainActivity.java
+++ app/src/main/java/com/elexandro/openplaystore/MainActivity.java
@@ -2,7 +2,11 @@ package com.elexandro.openplaystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openPlayStore = findViewById(R.id.button_open_play_store);
+        openPlayStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPlayStore();
+            }
+        });
+    }
+
+    private void openPlayStore() {
+        String appPackageName = "com.google.android.youtube";
+        Intent playStoreAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName));
+        Intent playStoreSiteIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName));
+
+        try {
+            startActivity(playStoreAppIntent);
+        } catch (Exception exception) {
+            startActivity(playStoreSiteIntent);
+        }
     }
 }
