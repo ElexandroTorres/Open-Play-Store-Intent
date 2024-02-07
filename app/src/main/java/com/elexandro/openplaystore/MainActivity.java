eb9a65ba650f6b2f3c2607f7a2ef36f9b05643c0
ElexandroTorres <elexandro.torres@gmail.com>
2024-02-07 13:03:42 -0300

    changes: ajustement in list of apps

--- app/src/main/java/com/elexandro/openplaystore/MainActivity.java
+++ app/src/main/java/com/elexandro/openplaystore/MainActivity.java
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -22,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         packageNames = new ArrayList<>();
-
         packageNames.add("com.google.android.youtube");
         packageNames.add("org.wikipedia");
         packageNames.add("com.mojang.minecraftpe");
@@ -45,6 +41,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception exception) {
             startActivity(playStoreSiteIntent);
         }
-
     }
 }
